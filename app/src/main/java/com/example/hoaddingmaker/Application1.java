cd51dfdbe1a64ca2d2d5c27df5660fe94094c141
Savita Dadsena <saividadsena18@gmail.com>
2022-08-09 16:04:49 +0530

    Final Commit

---
+++ app/src/main/java/com/example/hoaddingmaker/Application1.java
@@ -0,0 +1,14 @@
+package com.example.hoaddingmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Application1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application1);
+    }
+}
